ce79608718101ad5fa8d92cdb73e7b7cbc7520a0
Jess <jess.harrelson0707@gmail.com>
2016-11-15 13:33:29 -0500

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const list =
     React.createElement('h1', {}, 'My favorite ice cream flavors'),
     React.createElement('ul', {},
       [
-        React.createElement('li', { class: 'brown' }, 'Chocolate'),
-        React.createElement('li', { class: 'white' }, 'Vanilla'),
-        React.createElement('li', { class: 'yellow' }, 'Banana')
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
     ));
 
 ReactDOM.render(list, document.getElementById('main'));
+
+
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+
+ReactDOM.render(
+  container,
+  document.getElementById('main')
+);
+
+
